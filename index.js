585f86a5dd0f4ea9cef0bc8a8d42bce47ad1a655
Vince Tse <vincetse@users.noreply.github.com>
2020-03-19 22:08:58 +0000

    Use cowsay

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const cowsay = require("cowsay");
 const FortuneCookie = require("./fortune-cookie");
 
 try {
   let fortuneCookie = new FortuneCookie();
   let fortune = fortuneCookie.get();
-  console.log("Your fortune cookie says: " + fortune);
+  console.log(cowsay.say({
+    text: fortune,
+  }));
 } catch (error) {
   core.setFailed(error.message);
 }
